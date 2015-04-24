5f0676a24082660935aab71b0268ebf533333696
Alexandr Krylovskiy <alexandr.krylovskiy@gmail.com>
2015-04-24 14:13:01 +0200

    Updated godeps after merge

--- Godeps/_workspace/src/github.com/gorilla/mux/mux_test.go
+++ Godeps/_workspace/src/github.com/gorilla/mux/mux_test.go
@@ -9,7 +9,7 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/gorilla/context"
+	"linksmart.eu/services/historical-datastore/Godeps/_workspace/src/github.com/gorilla/context"
 )
 
 type routeTest struct {
