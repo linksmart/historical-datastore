02c46452e13c69b630f517040f58ab454147d0d6
Farshid Tavakolizadeh <tavakoli@fit.fraunhofer.de>
2015-05-11 19:03:58 +0200

    http tests

--- registry/http_test.go
+++ registry/http_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"math"
 	"math/rand"
@@ -39,6 +40,20 @@ func setupAPI() (*RegistryAPI, Client) {
 	registryClient := NewLocalClient(regStorage)
 
 	return regAPI, registryClient
+}
+
+// Manually send an HTTP request and get the response
+func httpRequestClient(method string, url string, body io.Reader) (*http.Response, error) {
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+	//req.Header.Set("Content-Type", "")
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
 }
 
 func TestHttpIndex(t *testing.T) {
@@ -120,15 +135,19 @@ func TestHttpIndex(t *testing.T) {
 		for i := 0; i < inThisPage; i++ {
 			dummyDS := dummyDSs[i]
 			returnedDS := reg.Entries[i]
-			if dummyDS.ID != returnedDS.ID {
-				t.Errorf("Mismatched datasource: \n%v \n%v\n", dummyDSs[i], reg.Entries[i])
+			dummyDS_b, _ := json.Marshal(dummyDS)
+			returnedDS_b, _ := json.Marshal(returnedDS)
+			
+			// compare them
+			if string(dummyDS_b) != string(returnedDS_b) {
+				t.Errorf("Mismatch retrieved:\n%s\n and stored:\n%s\n", string(dummyDS_b), string(returnedDS_b))
 			}
 		}
 
 		totalReturnedDS += len(reg.Entries)
 	}
 
-	// Compare total created with total datasources in all pages of registry
+	// Compare the total number of created and retrieved(in all pages of registry) data sources
 	if totalReturnedDS != totalDummy {
 		t.Errorf("Mismatched total created(%d) and returned(%d) data sources!", totalDummy, totalReturnedDS)
 	}
@@ -137,62 +156,40 @@ func TestHttpIndex(t *testing.T) {
 }
 
 func TestHttpCreate(t *testing.T) {
-	regAPI, _ := setupAPI()
+	regAPI, registryClient := setupAPI()
+	MIMEType := "unknown/unknown"
 
 	ts := httptest.NewServer(setupRouter(regAPI))
 	defer ts.Close()
 
-	b := []byte(`
-		{
-			"resource": "http://any-domain:8080/1234",
-			"meta": {},
-			"retention": {
-			    "policy": "2h",
-			    "duration": "3d"
-			},
-			"aggregation": [],
-			"type": "string",
-			"format": "any_format"
-		}
-		`)
-
 	// try bad payload
-	res, err := http.Post(ts.URL+"/registry", "unknown/unknown", bytes.NewReader([]byte{0xde, 0xad}))
+	res, err := http.Post(ts.URL+"/registry", MIMEType, bytes.NewReader([]byte{0xde, 0xad}))
 	if err != nil {
-		t.Errorf(err.Error())
+		t.Fatalf(err.Error())
 	}
-
 	if res.StatusCode != http.StatusBadRequest {
 		t.Errorf("Server response is not %v but %v", http.StatusBadRequest, res.StatusCode)
 	}
+	res.Body.Close()
+
+	// try invalid bodies
+	for _, invalidBodyStr := range append(invalidBodies, invalidPostBodies...) {
+		invalidBody := []byte(invalidBodyStr)
+
+		res, err := http.Post(ts.URL+"/registry", MIMEType, bytes.NewReader(invalidBody))
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+		if res.StatusCode != http.StatusConflict {
+			t.Fatalf("Server response is not %v but %v :\n%v", http.StatusConflict, res.StatusCode, invalidBodyStr)
+		}
+		res.Body.Close()
+	}
 
 	// try a good one
-	res, err = http.Post(ts.URL+"/registry", "unknown/unknown", bytes.NewReader(b))
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-
-	if res.StatusCode != http.StatusCreated {
-		t.Errorf("Server response is not %v but %v", http.StatusCreated, res.StatusCode)
-	}
-
-	//	body, err := ioutil.ReadAll(res.Body)
-	//	defer res.Body.Close()
-	//	if err != nil {
-	//		t.Errorf(err.Error())
-	//	}
-	//t.Log(string(body))
-}
-
-// Create a data source and retrieve it back
-func TestHttpRetrieve(t *testing.T) {
-	regAPI, _ := setupAPI()
-	ts := httptest.NewServer(setupRouter(regAPI))
-	defer ts.Close()
-
 	b := []byte(`
 		{
-			"resource": "http://any-domain:8080/1234",
+			"resource": "any_url",
 			"meta": {},
 			"retention": {
 			    "policy": "1w",
@@ -203,85 +200,187 @@ func TestHttpRetrieve(t *testing.T) {
 			"format": "any_format"
 		}
 		`)
-
-	// first, create a data source
-	res, err := http.Post(ts.URL+"/registry", "unknown/unknown", bytes.NewReader(b))
+	res, err = http.Post(ts.URL+"/registry", MIMEType, bytes.NewReader(b))
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
-
+	if res.StatusCode != http.StatusCreated {
+		t.Errorf("Server response is not %v but %v", http.StatusCreated, res.StatusCode)
+	}
 	// Get response's url (including new uuid)
 	url, err := res.Location()
-	t.Log("[GET] " + ts.URL + url.Path)
-
-	// Retrieve what it was created
-	res, err = http.Get(ts.URL + url.Path)
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
+	res.Body.Close()
 
-	body, err := ioutil.ReadAll(res.Body)
+	// Extract id from url
+	splitURL := strings.Split(url.Path, "/")
+	if len(splitURL) != 3 {
+		t.Fatal("Invalid url in Location header-entry")
+	}
+	id := splitURL[2]
+
+	// Manually construct the expected POSTed data source
+	var postedDS DataSource
+	err = json.Unmarshal(b, &postedDS)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	postedDS.ID = id
+	postedDS.URL = fmt.Sprintf("%s/%s", common.RegistryAPILoc, postedDS.ID)
+	postedDS.Data = fmt.Sprintf("%s/%s", common.DataAPILoc, postedDS.ID)
+	postedDS_b, _ := json.Marshal(&postedDS)
+
+	// Retrieve the added data source
+	addedDS, _ := registryClient.Get(id)
+	addedDS_b, _ := json.Marshal(&addedDS)
+
+	// compare posted and added data sources
+	if string(postedDS_b) != string(addedDS_b) {
+		t.Errorf("Mismatch POSTed:\n%s\n and added data:\n%s\n", string(postedDS_b), string(addedDS_b))
+	}
+
+	return
+}
+
+// Create a data source and retrieve it back
+func TestHttpRetrieve(t *testing.T) {
+	regAPI, registryClient := setupAPI()
+	ts := httptest.NewServer(setupRouter(regAPI))
+	defer ts.Close()
+
+	ID := GenerateDummyData(1, registryClient)[0]
+	aDataSource, _ := registryClient.Get(ID)
+
+	// Retrieve what it was created
+	res, err := http.Get(fmt.Sprintf("%v%s/%s", ts.URL, common.RegistryAPILoc, ID))
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	b, err := ioutil.ReadAll(res.Body)
 	defer res.Body.Close()
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
 
-	// get id from url
-	uuid := strings.Split(url.Path, "/")[2]
-
-	// Add generated uuid to the data source
-	var ds DataSource
-	err = json.Unmarshal(body, &ds)
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-	ds.ID = uuid
-	ds.URL = fmt.Sprintf("%s/%s", common.RegistryAPILoc, ds.ID)
-	ds.Data = fmt.Sprintf("%s/%s", common.DataAPILoc, ds.ID)
-	get_b, err := json.Marshal(&ds)
+	// marshal the stored data source for comparison
+	storedDS_b, err := json.Marshal(&aDataSource)
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
 
-	// compare created data with the source
-	if string(body) != string(get_b) {
-		t.Errorf("Unexpected response: %s", string(body))
+	// compare stored and retrieved(GET) data sources
+	if string(storedDS_b) != string(b) {
+		t.Errorf("Mismatch retrieved(GET):\n%s\n and stored data:\n%s\n", string(b), string(storedDS_b))
 	}
 }
 
 func TestHttpUpdate(t *testing.T) {
-	t.Skip("TODO: API handler test")
+	regAPI, registryClient := setupAPI()
+
+	ts := httptest.NewServer(setupRouter(regAPI))
+	defer ts.Close()
+
+	// Create a dummy data source
+	ID := GenerateDummyData(1, registryClient)[0]
+	url := ts.URL + "/registry/" + ID
+
+	// try bad payload
+	res, err := httpRequestClient("PUT", url, bytes.NewReader([]byte{0xde, 0xad}))
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if res.StatusCode != http.StatusBadRequest {
+		t.Errorf("Server response is not %v but %v", http.StatusBadRequest, res.StatusCode)
+	}
+	res.Body.Close()
+
+	// try invalid bodies
+	for _, invalidBodyStr := range append(invalidBodies, invalidPutBodies...) {
+		invalidBody := []byte(invalidBodyStr)
+
+		res, err := httpRequestClient("PUT", url, bytes.NewReader(invalidBody))
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+		if res.StatusCode != http.StatusConflict {
+			t.Fatalf("Server response is not %v but %v :\n%v", http.StatusConflict, res.StatusCode, invalidBodyStr)
+		}
+		res.Body.Close()
+	}
+
+	// try a good one
+	b := []byte(`
+		{
+			"meta": {},
+			"retention": {
+			    "policy": "1w",
+			    "duration": "1m"
+			},
+			"aggregation": [],
+			"format": "any_format"
+		}
+		`)
+	res, err = httpRequestClient("PUT", url, bytes.NewReader(b))
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Server response is not %v but %v", http.StatusOK, res.StatusCode)
+	}
+	res.Body.Close()
+
+	// Retrieve the updated data source
+	updatedDS, _ := registryClient.Get(ID)
+	updatedDS_b, _ := json.Marshal(&updatedDS)
+
+	// Manually construct the expected updated(PUT) data source
+	var putDS DataSource
+	err = json.Unmarshal(b, &putDS)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	putDS.ID = ID
+	putDS.URL = fmt.Sprintf("%s/%s", common.RegistryAPILoc, putDS.ID)
+	putDS.Data = fmt.Sprintf("%s/%s", common.DataAPILoc, putDS.ID)
+	putDS.Resource = updatedDS.Resource
+	putDS.Type = updatedDS.Type
+	putDS_b, _ := json.Marshal(&putDS)
+
+	// compare updated(PUT) data source with the one in memory
+	if string(putDS_b) != string(updatedDS_b) {
+		t.Errorf("Mismatch PUT:\n%s\n and updated data:\n%s\n", string(putDS_b), string(updatedDS_b))
+	}
 }
 
 func TestHttpDelete(t *testing.T) {
 	regAPI, registryClient := setupAPI()
 
-	// Create some dummy data
-	IDs := GenerateDummyData(5, registryClient)
-	//aDataSource := registryClient.Get(IDs[0])
+	// Create a dummy data source
+	ID := GenerateDummyData(1, registryClient)[0]
 
 	ts := httptest.NewServer(setupRouter(regAPI))
 	defer ts.Close()
 
-	// Try an existing item
-	url := ts.URL + "/registry/" + IDs[0]
-	req, err := http.NewRequest("DELETE", url, nil)
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-	res, err := http.DefaultClient.Do(req)
+	// Try deleting an existing item
+	url := ts.URL + "/registry/" + ID
+	res, err := httpRequestClient("DELETE", url, nil)
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
 	if res.StatusCode != http.StatusOK {
 		t.Fatalf("Server response is %v instead of %v", res.StatusCode, http.StatusOK)
 	}
+	// check whether it is deleted
+	_, err = registryClient.Get(ID)
+	if err == nil {
+		t.Fatalf("Server responded %v but data source is not deleted!", res.StatusCode)
+	}
 
-	// TODO check whether it is deleted?
-
-	// Try a non-existing item
+	// Try deleting a non-existing item
 	url = ts.URL + "/registry/" + "f5e0a314-0c8c-4938-9961-74625c6614da"
-	req, err = http.NewRequest("DELETE", url, nil)
+	req, err := http.NewRequest("DELETE", url, nil)
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
@@ -321,3 +420,139 @@ func GenerateDummyData(quantity int, c Client) []string {
 
 	return IDs
 }
+
+// A pool of bad data sources
+var (
+	invalidBodies = []string{
+		// Provided id //////////
+		`{
+			"id": "12345",
+			"resource": "any_url",
+			"retention": {
+			    "policy": "2h",
+			    "duration": "3d"
+			},
+			"type": "string",
+			"format": "any_format"
+		}`,
+		// Provided url //////////
+		`{
+			"url": "any_regurl",
+			"resource": "any_url",
+			"retention": {
+			    "policy": "2h",
+			    "duration": "3d"
+			},
+			"type": "string",
+			"format": "any_format"
+		}`,
+		// Provided data url //////////
+		`{
+			"data" : "any_dataurl",
+			"resource": "any_url",
+			"retention": {
+			    "policy": "2h",
+			    "duration": "3d"
+			},
+			"type": "string",
+			"format": "any_format"
+		}`,
+		// Invalid retention policy //////////
+		`{
+			"resource": "any_url",
+			"retention": {
+			    "policy": "2",
+			    "duration": "3d"
+			},
+			"type": "string",
+			"format": "any_format"
+		}`,
+		// Invalid retention duration //////////
+		`{
+			"resource": "any_url",
+			"retention": {
+			    "policy": "2h",
+			    "duration": "3s"
+			},
+			"type": "string",
+			"format": "any_format"
+		}`,
+		// Missing format //////////
+		`{
+			"resource": "any_url",
+			"retention": {
+			    "policy": "2h",
+			    "duration": "3w"
+			},
+			"type": "string",
+			"format": ""
+		}`,
+		//		// Float type and missing aggregation //////////
+		//		`{
+		//			"resource": "any_url",
+		//			"meta": {},
+		//			"retention": {
+		//			    "policy": "2h",
+		//			    "duration": "3w"
+		//			},
+		//			"aggregation": [],
+		//			"type": "float",
+		//			"format": "any_format"
+		//		}`,
+
+	}
+
+	invalidPostBodies = []string{
+		// Missing resource url //////////
+		`{
+			"resource": "",
+			"retention": {
+			    "policy": "2h",
+			    "duration": "3d"
+			},
+			"type": "string",
+			"format": "any_format"
+		}`,
+		// Missing type //////////
+		`{
+			"resource": "any_url",
+			"retention": {
+			    "policy": "2h",
+			    "duration": "3d"
+			},
+			"type": "",
+			"format": "any_format"
+		}`,
+		// Invalid type //////////
+		`{
+			"resource": "any_url",
+			"retention": {
+			    "policy": "2h",
+			    "duration": "3w"
+			},
+			"type": "some_unsupported_type",
+			"format": "any_format"
+		}`,
+	}
+
+	invalidPutBodies = []string{
+		// Provided read-only resource url //////////
+		`{
+			"resource": "any_url",
+			"retention": {
+			    "policy": "2h",
+			    "duration": "3d"
+			},
+			"format": "any_format"
+		}`,
+		// Provided read-only type //////////
+		`{
+			"retention": {
+			    "policy": "2h",
+			    "duration": "3d"
+			},
+			"type": "string",
+			"format": "any_format"
+		}`,
+	}
+)
