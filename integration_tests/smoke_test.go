c83bfa57c680ea3ece36dc442e45420f54ee7515
Farshid Tavakolizadeh <email@farshid.ws>
2018-05-07 01:43:39 +0200

    added concurrent create and update tests

--- integration_tests/smoke_test.go
+++ integration_tests/smoke_test.go
@@ -6,6 +6,8 @@ import (
 	"sync"
 	"testing"
 
+	"github.com/satori/go.uuid"
+
 	"code.linksmart.eu/hds/historical-datastore/data"
 
 	"code.linksmart.eu/hds/historical-datastore/registry"
@@ -15,6 +17,137 @@ import (
 const endpoint = "http://localhost:8085"
 const registryEndpoint = endpoint + "/registry"
 const dataEndpoint = endpoint + "/data"
+
+func TestConcurrentCreates(t *testing.T) {
+	registryClient, err := registry.NewRemoteClient(registryEndpoint, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var entries []*registry.DataSource
+
+	// create many concurrently
+	const TOTAL = 10
+	for i := 0; i < TOTAL; i++ {
+		ds := &registry.DataSource{
+			Resource: fmt.Sprintf("dummy/%s", uuid.NewV4().String()),
+			Aggregation: []registry.Aggregation{
+				{
+					Interval:   "1h",
+					Aggregates: []string{"max", "min"},
+				},
+				{
+					Interval:   "1h",
+					Aggregates: []string{"mean", "stddev"},
+				},
+			},
+			Type:   "float",
+			Format: "application/senml+json",
+		}
+		entries = append(entries, ds)
+	}
+
+	idCollector := make(chan string, TOTAL)
+	var wg sync.WaitGroup
+	for i := 0; i < TOTAL; i++ {
+		wg.Add(1)
+		go func(thisDS *registry.DataSource) {
+			defer wg.Done()
+			id, err := registryClient.Add(thisDS)
+			if err != nil {
+				t.Fatal(err)
+			}
+			idCollector <- id
+		}(entries[i])
+	}
+	wg.Wait()
+	close(idCollector)
+
+	c := 0
+	for id := range idCollector {
+		_, err := registryClient.Get(id)
+		if err != nil {
+			t.Fatal(err)
+		}
+		c++
+	}
+	if c != TOTAL {
+		t.Fatalf("Total created %d instead of %d", c, TOTAL)
+	}
+}
+
+func TestConcurrentUpdates(t *testing.T) {
+	registryClient, err := registry.NewRemoteClient(registryEndpoint, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dataClient, err := data.NewRemoteClient(dataEndpoint, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var entries []*registry.DataSource
+
+	// create many
+	const TOTAL = 10
+	for i := 0; i < TOTAL; i++ {
+		ds := registry.DataSource{
+			Resource: fmt.Sprintf("dummy/%s", uuid.NewV4().String()),
+			Aggregation: []registry.Aggregation{
+				{
+					Interval:   "1h",
+					Aggregates: []string{"max", "min"},
+				},
+				{
+					Interval:   "1h",
+					Aggregates: []string{"mean", "stddev"},
+				},
+			},
+			Type:   "float",
+			Format: "application/senml+json",
+		}
+		ds.ID, err = registryClient.Add(&ds)
+		if err != nil {
+			t.Fatal(err)
+		}
+		addedDS, err := registryClient.Get(ds.ID)
+		if err != nil {
+			t.Fatal(err)
+		}
+		entries = append(entries, addedDS)
+	}
+
+	// send some data
+	for _, ds := range entries {
+		var senml gosenml.Message
+		for i := 0; i < 100; i++ {
+			v := float64(i)
+			senml.Entries = append(senml.Entries, gosenml.Entry{Name: ds.Resource, Value: &v})
+		}
+		b, _ := json.Marshal(senml)
+		err := dataClient.Submit(b, "application/senml+json", ds.ID)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// update all concurrently
+	var wg sync.WaitGroup
+	for i := 0; i < TOTAL; i++ {
+		wg.Add(1)
+		go func(thisDS *registry.DataSource) {
+			defer wg.Done()
+			thisDS.Aggregation = nil
+			err := registryClient.Update(thisDS.ID, thisDS)
+			if err != nil {
+				t.Fatal(err)
+			}
+		}(entries[i])
+	}
+
+	wg.Wait()
+}
 
 func TestConcurrentDeletes(t *testing.T) {
 	registryClient, err := registry.NewRemoteClient(registryEndpoint, nil)
@@ -27,55 +160,35 @@ func TestConcurrentDeletes(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	dsJSON := []byte(`{
-            "resource": "",
-            "retention": "",
-            "aggregation": [
-                {
-                    "interval": "1h",
-                    "aggregates": [
-                        "max",
-                        "min"
-                    ],
-                    "retention": ""
-                },
-                {
-                    "interval": "1h",
-                    "aggregates": [
-                        "mean",
-                        "stddev"
-                    ],
-                    "retention": ""
-                }
-            ],
-            "type": "float",
-            "format": "application/senml+json"
-        }`)
-
-	var ds registry.DataSource
-	err = json.Unmarshal(dsJSON, &ds)
-	if err != nil {
-		t.Fatal(err)
-	}
+	var entries []registry.DataSource
 
 	// create many
-	for i := 0; i < 10; i++ {
-		ds.Resource = fmt.Sprintf("dummy/%d", i)
-
-		_, err = registryClient.Add(&ds)
-		if err != nil {
-			t.Log(err)
+	const TOTAL = 10
+	for i := 0; i < TOTAL; i++ {
+		ds := registry.DataSource{
+			Resource: fmt.Sprintf("dummy/%s", uuid.NewV4().String()),
+			Aggregation: []registry.Aggregation{
+				{
+					Interval:   "1h",
+					Aggregates: []string{"max", "min"},
+				},
+				{
+					Interval:   "1h",
+					Aggregates: []string{"mean", "stddev"},
+				},
+			},
+			Type:   "float",
+			Format: "application/senml+json",
 		}
-	}
-
-	// retrieve them
-	reg, err := registryClient.Index(1, 100)
-	if err != nil {
-		t.Fatal(err)
+		ds.ID, err = registryClient.Add(&ds)
+		if err != nil {
+			t.Fatal(err)
+		}
+		entries = append(entries, ds)
 	}
 
 	// send some data
-	for _, ds := range reg.Entries {
+	for _, ds := range entries {
 		var senml gosenml.Message
 		for i := 0; i < 100; i++ {
 			v := float64(i)
@@ -84,13 +197,13 @@ func TestConcurrentDeletes(t *testing.T) {
 		b, _ := json.Marshal(senml)
 		err := dataClient.Submit(b, "application/senml+json", ds.ID)
 		if err != nil {
-			t.Log(err)
+			t.Fatal(err)
 		}
 	}
 
 	// delete all concurrently
 	var wg sync.WaitGroup
-	for _, ds := range reg.Entries {
+	for i := 0; i < TOTAL; i++ {
 		wg.Add(1)
 		go func(id string) {
 			defer wg.Done()
@@ -98,7 +211,7 @@ func TestConcurrentDeletes(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-		}(ds.ID)
+		}(entries[i].ID)
 	}
 
 	wg.Wait()
