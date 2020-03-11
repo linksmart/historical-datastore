1751f203909d2f03fcc4f4bac63e2fa126c6693e
Shreekantha Devasya <shreekantha.devasya@fit.fraunhofer.de>
2020-03-11 09:31:44 +0100

    SQLite changes

--- data/sqldb.go
+++ data/sqldb.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/farshidtz/senml"
 	"github.com/linksmart/historical-datastore/common"
@@ -63,29 +64,102 @@ func (s *SqlStorage) Submit(data map[string]senml.Pack, sources map[string]*regi
 	return nil
 }
 
+//This function converts a floating point number (which is supported by senml) to a bytearray
+func floatTimeToInt64(senmlTime float64) int64 {
+	return int64(senmlTime * (1e9)) //time.Unix(int64(sec), int64(frac*(1e9))).UnixNano()
+}
+
+//This function converts a bytearray floating point number (which is supported by senml)
+func int64ToFloatTime(timeVal int64) float64 {
+	return float64(timeVal) / 1e9
+}
+
+func ToSenmlTime(t time.Time) float64 {
+	if t.IsZero() {
+		return 0
+	}
+	return int64ToFloatTime(t.UnixNano())
+}
+
+func FromSenmlTime(t float64) time.Time {
+	return time.Unix(0, floatTimeToInt64(t))
+}
+
 func (s *SqlStorage) Query(q Query, sources ...*registry.DataStream) (pack senml.Pack, total int, nextOffset *int, err error) {
 
 	//perItems, offsets := common.PerItemPagination(q.Limit, q.Page, q.PerPage, len(sources))
-
-	//for index, source := range sources {
-		partStmt := fmt.Sprintf("FROM [%s] WHERE time ORDER BY time %s LIMIT %d OFFSET %d", source.Name, q.Sort, perItems[index], offsets[index])
-		//return nil, 0, nil, fmt.Errorf("not implemented")
-		if q.count {
-			stmt := "SELECT COUNT(*) " + partStmt
-			row := s.pool.QueryRow(stmt)
-			err := row.Scan(&total)
+	var unionStmt strings.Builder
+	if len(sources) == 1 {
+		unionStmt.WriteString(fmt.Sprintf("[%s] where time BETWEEN %d and %d LIMIT %d OFFSET %d",
+			sources[0].Name, ToSenmlTime(q.From), ToSenmlTime(q.To), q.PerPage, (q.Page-1)*q.PerPage, q.Sort))
+	} else {
+		unionStmt.WriteByte('(')
+		unionStr := ""
+		for _, source := range sources {
+			unionStmt.WriteString(fmt.Sprintf("%sSELECT ([%s] as table_name , %s as type, time, value) FROM [%s] WHERE time BETWEEN %d and %d", unionStr, source.Name, source.Type, source.Name, ToSenmlTime(q.From), ToSenmlTime(q.To)))
+			unionStr = " UNION ALL "
+		}
+		unionStmt.WriteByte(')')
+	}
+	var stmt string
+	if q.count {
+		stmt = "SELECT COUNT(*) FROM " + unionStmt.String()
+		row := s.pool.QueryRow(stmt)
+		err := row.Scan(&total)
+		if err != nil {
+			return nil, 0, nil, fmt.Errorf("error while querying count:%s", err)
+		}
+	}
+	stmt = fmt.Sprintf("SELECT * FROM %s  ORDER BY time %s LIMIT %d OFFSET %d", unionStmt.String(), q.Sort, q.PerPage, (q.Page-1)*q.PerPage)
+	rows, err := s.pool.Query(stmt)
+	if err != nil {
+		return nil, 0, nil, fmt.Errorf("error while querying rows:%s", err)
+	}
+	defer rows.Close()
+	records := make([]senml.Record, q.PerPage)
+	if len(sources) == 1 {
+		var timeVal float64
+		switch sources[0].Type {
+		case common.FLOAT:
+			floatVal := val.(float64)
+			records = append(records, senml.Record{Name: tableName, Value: &floatVal, Time: timeVal})
+		case common.STRING:
+			stringVal := val.(string)
+			records = append(records, senml.Record{Name: tableName, StringValue: stringVal, Time: timeVal})
+		case common.BOOL:
+			boolVal := val.(bool)
+			records = append(records, senml.Record{Name: tableName, BoolValue: boolVal, Time: timeVal})
+		}
+		for rows.Next() {
+			err = rows.Scan(&timeVal, &val)
 			if err != nil {
-				return nil, 0, nil, fmt.Errorf("error while querying count:%s", err)
+				return nil, 0, nil, fmt.Errorf("error while scanning query results:%s", err)
 			}
 		}
+	} else {
+		var tableName string
+		var timeVal float64
+		var val interface{}
+		var typestr string
+		for rows.Next() {
 
-
-		stmt := fmt.Sprintf("SELECT (time, value) FROM [%s] WHERE time ORDER BY time %s LIMIT %d OFFSET %d ",
-			ds, strings.Join(valueStrings, ","))
-		_, err := s.pool.Exec(stmt, valueArgs...)
+			err = rows.Scan(&tableName, &typestr, &timeVal, &val)
+			if err != nil {
+				return nil, 0, nil, fmt.Errorf("error while scanning query results:%s", err)
+			}
+			switch typestr {
+			case common.FLOAT:
+				floatVal := val.(float64)
+				records = append(records, senml.Record{Name: tableName, Value: &floatVal, Time: timeVal})
+			case common.STRING:
+				stringVal := val.(string)
+				records = append(records, senml.Record{Name: tableName, StringValue: stringVal, Time: timeVal})
+			case common.BOOL:
+				boolVal := val.(bool)
+				records = append(records, senml.Record{Name: tableName, BoolValue: boolVal, Time: timeVal})
+			}
+		}
 	}
-
-	return err
 }
 
 func (s *SqlStorage) Disconnect() error {
